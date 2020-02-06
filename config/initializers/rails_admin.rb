15d26a242260d206f9c5935a989ef93a1258b035
watabouyosida <58410391+watabouyosida@users.noreply.github.com>
2020-02-06 13:52:43 +0900

    adomin basic認証

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,9 @@
 RailsAdmin.config do |config|
-
+  config.authenticate_with do
+    authenticate_or_request_with_http_basic('Site Message') do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
   ### Popular gems integration
 
   ## == Devise ==
